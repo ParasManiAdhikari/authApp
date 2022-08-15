028c58d6fb6949f83e3f3be13d8592bcd3276c40
Paras Adhikari <paras.adhikari@stud.th-luebeck.de>
2022-08-15 19:08:16 +0200

    firebase login functionality added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,62 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import LoginScreen from './LoginScreen'
+import { customerEmail } from './LoginScreen'
+import { TouchableOpacity } from 'react-native-web'
+import { auth } from '../firebase'
+import { useNavigation } from '@react-navigation/core';
+
+
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  const handleSingOut = () => {
+    auth.signOut()
+      .then(() => {
+        navigation.replace('Login')
+      })
+      .catch(error => {
+        alert(error.message)
+      }
+      )
+  }
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.buttonText}>Welcome to your Homepage!</Text>
+      <Text style={styles.buttonText}>Your email is : {auth.currentUser?.email} </Text>
+      <TouchableOpacity
+        onPress={handleSingOut}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Sign Out</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    width: '30%',
+    height: 50,
+    backgroundColor: 'white',
+    marginTop: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    borderColor: 'black',
+    borderWidth: 2,
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    color: 'black',
+  },
+})
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native'
 // import { auth } from '../firebase'
 import { auth } from '../firebase';
+import { useNavigation } from '@react-navigation/core';
+
 
 const LoginScreen = () => {
     const [email, setEmail] = React.useState('')
     const [password, setPassword] = React.useState('')
+
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            if (user) {
+                navigation.replace("Home")
+            }
+        })
+        return unsubscribe;
+    }, [])
 
     const handleSignUp = () => {
         auth
@@ -18,14 +31,32 @@ const LoginScreen = () => {
                 if (error.code === 'auth/email-already-in-use') {
                     alert('That email address is already in use!');
                 }
-
                 if (error.code === 'auth/invalid-email') {
                     alert('That email address is invalid!');
                 }
-
+                if (error.code === 'auth/missing-email') {
+                    alert('No email address!');
+                }
                 console.error(error);
             });
-            console.log(email, password);
+        console.log(email, password);
+    }
+
+    const handleLogin = () => {
+        auth.signInWithEmailAndPassword(email, password)
+            .then(() => {
+                customerEmail = email;
+                alert('You are Logged in!');
+            })
+            .catch(error => {
+                if (error.code === 'auth/user-not-found') {
+                    alert('User not found!');
+                }
+                if (error.code === 'auth/wrong-password') {
+                    alert('Wrong password!');
+                }
+                console.error(error);
+            });
     }
 
     return (
@@ -48,14 +79,14 @@ const LoginScreen = () => {
             </View>
             <View style={styles.buttonContainer}>
                 <TouchableOpacity
-                    onPress={() => { }}
+                    onPress={() => { handleLogin() }}
                     style={styles.button}
                 >
                     <Text style={styles.buttonText}>Login</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity
-                    onPress={() => {handleSignUp()}}
+                    onPress={() => { handleSignUp() }}
                     style={[styles.button, styles.registerButton]}
                 >
                     <Text style={styles.registerButtonText}>Register</Text>
